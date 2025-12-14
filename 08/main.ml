9bf074f4375df925a6ce6b898e834a533ac3fbdf
Mark Shevchenko <markshevchenko@gmail.com>
2025-12-14 22:32:51 +0300

    Сделал 8.1

--- 08/main.ml
+++ 08/main.ml
@@ -9,88 +9,112 @@ let lines_of_file filename =
   in
   Seq.unfold next_line ()
 
-let rec build_pairs element list accumulator =
-  match list with
-  | [] -> accumulator
-  | x::xs -> build_pairs element xs ((element, x)::accumulator)
+let square_distance (x1, y1, z1) (x2, y2, z2) =
+  let square x = x * x in
+  square (x2 - x1) + square (y2 - y1) + square (z2 - z1)
 
-let rec diagonal_pairwise list accumulator =
-  match list with
-  | [] -> failwith "empty list"
-  | [_] -> failwith "too short list"
-  | x1::[x2] -> (x1, x2)::accumulator
-  | x::xs -> diagonal_pairwise xs (build_pairs x xs accumulator)
+let distance_compare (p1, p2) (p3, p4) =
+  Int.compare (square_distance p1 p2) (square_distance p3 p4)
 
-let pairwise list =
-  diagonal_pairwise list []
+let make_pairs points =
+  let pairs_length = Array.length points * (Array.length points - 1) / 2 in
+  let pairs = Array.make pairs_length ((0,0,0), (0,0,0)) in
+  let index = ref 0 in
+  for i = 0 to Array.length points - 2 do
+    for j = i + 1 to Array.length points - 1 do
+      pairs.(!index) <- (points.(i), points.(j));
+      index := !index + 1;
+    done
+  done;
+  Array.sort distance_compare pairs;
+  pairs
 
-let compare ((x11, y11, z11), (x12, y12, z12)) ((x21, y21, z21), (x22, y22, z22)) =
-  let d1 = (x12 - x11) * (x12 - x11) + (y12 - y11) * (y12 - y11) + (z12 - z11) * (z12 - z11) in
-  let d2 = (x22 - x21) * (x22 - x21) + (y22 - y21) * (y22 - y21) + (z22 - z21) * (z22 - z21) in
-  d1 - d2
-
-let group_by_key f lst =
-  List.fold_left (fun acc x ->
-    let key = f x in
-    match List.assoc_opt key acc with
-    | Some vals -> (key, x :: vals) :: List.remove_assoc key acc
-    | None -> (key, [x]) :: acc
-  ) [] lst
-
-let solution1 max_connections points =
-  let pairs =
-    pairwise points
-    |> List.filter (fun ((x1, y1, z1), (x2, y2, z2)) -> x1 != x2 || y1 != y2 || z1 != z2)
-    |> List.sort compare in
-  let cirtuits = Hashtbl.create 1000 in
-  let result () =
-    let l1 = cirtuits
-    |> Hashtbl.to_seq_values
-    |> List.of_seq in
-    List.iter (Printf.printf " %d") l1;
-    Printf.printf "\n";
-    let l2 = group_by_key (Fun.id) l1
-    |> List.map (fun (_, list) -> List.length list) in
-    List.iter (Printf.printf " %d") l2;
-    Printf.printf "\n";
-    let l3 = List.sort_uniq (fun a b -> b - a) l2 in
-    List.iter (Printf.printf " %d") l3;
-    Printf.printf "\n";
-    l3 |> List.take 3 |> List.fold_left ( * ) 1
-  in
-  let set_connections p1 p2 current_connection =
-    match (Hashtbl.find_opt cirtuits p1, Hashtbl.find_opt cirtuits p2) with
-    | (None, None) ->
-        Printf.printf "both: %d\n" current_connection;
-        Hashtbl.add cirtuits p1 current_connection;
-        Hashtbl.add cirtuits p2 current_connection
-    | (Some connection, None) ->
-        Printf.printf "right: %d\n" connection;
-        Hashtbl.add cirtuits p2 connection
-    | (None, Some connection) ->
-        Printf.printf "left: %d\n" connection;
-        Hashtbl.add cirtuits p1 connection
-    | (Some connection1, Some connection2) ->
-        Printf.printf "none: %d, %d, %d\n" connection1 connection2 current_connection;
-        ()
-  in
-  let rec process current_connection = function
-    | [] -> result ()
-    | ((x1, y1, z1), (x2, y2, z2))::ps ->
-      Printf.printf "(%d, %d, %d)-(%d, %d, %d)\n" x1 y1 z1 x2 y2 z2;
-      if current_connection > max_connections + 1 then
-        result ()
-      else begin
-        set_connections (x1, y1, z1) (x2, y2, z2) current_connection;
-        process (current_connection + 1) ps
+let solution1 connections points =
+  let pairs = make_pairs points in
+  let circuits = Hashtbl.create connections in
+  let cycles = Hashtbl.create connections in
+  for i = 0 to connections - 1 do
+    let point1 = fst pairs.(i) in
+    let point2 = snd pairs.(i) in
+    match (Hashtbl.find_opt circuits point1), (Hashtbl.find_opt circuits point2) with
+    | None, None ->
+      Hashtbl.add circuits point1 i;
+      Hashtbl.add circuits point2 i;
+      Hashtbl.add cycles i (point1::[point2]);
+    | None, Some connection ->
+      Hashtbl.add circuits point1 connection;
+      Hashtbl.replace cycles connection (point1::Hashtbl.find cycles connection);
+    | Some connection, None  ->
+      Hashtbl.add circuits point2 connection;
+      Hashtbl.replace cycles connection (point2::Hashtbl.find cycles connection);
+    | Some connection1, Some connection2 ->
+      if connection1 != connection2 then begin
+        let points1 = Hashtbl.find cycles connection1 in
+        let points2 = Hashtbl.find cycles connection2 in
+        List.iter (fun point -> Hashtbl.replace circuits point connection1) points2;
+        Hashtbl.replace cycles connection1 (points1 @ points2);
+        Hashtbl.remove cycles connection2;
       end
-  in
-  process 1 pairs
+  done;
+
+  cycles
+  |> Hashtbl.to_seq_values
+  |> Seq.map (fun list -> List.length list)
+  |> List.of_seq
+  |> List.sort_uniq (fun a b -> -Int.compare a b)
+  |> List.take 3
+  |> List.fold_left ( * ) 1
+
+let x_of (x, _, _) = x
+let print_points (x1, y1, z1) (x2, y2, z2) =
+  Printf.printf "(%d, %d, %d) - (%d, %d, %d)\n" x1 y1 z1 x2 y2 z2
+
+let solution2 points =
+  let point_set =
+    points
+    |> Array.to_seq
+    |> Seq.map (fun point -> (point, ()))
+    |> Hashtbl.of_seq in
+  let pairs = make_pairs points in
+  let circuits = Hashtbl.create 1000 in
+  let cycles = Hashtbl.create 1000 in
+  let i = ref 0 in
+  while Hashtbl.length point_set > 0 do
+    let point1 = fst pairs.(!i) in
+    let point2 = snd pairs.(!i) in
+    match (Hashtbl.find_opt circuits point1), (Hashtbl.find_opt circuits point2) with
+    | None, None ->
+      Hashtbl.add circuits point1 !i;
+      Hashtbl.add circuits point2 !i;
+      Hashtbl.add cycles !i (point1::[point2]);
+      Hashtbl.remove point_set point1;
+      Hashtbl.remove point_set point2;
+    | None, Some connection ->
+      Hashtbl.add circuits point1 connection;
+      Hashtbl.replace cycles connection (point1::Hashtbl.find cycles connection);
+      Hashtbl.remove point_set point1;
+    | Some connection, None  ->
+      Hashtbl.add circuits point2 connection;
+      Hashtbl.replace cycles connection (point2::Hashtbl.find cycles connection);
+      Hashtbl.remove point_set point2;
+    | Some connection1, Some connection2 ->
+      if connection1 != connection2 then begin
+        let points1 = Hashtbl.find cycles connection1 in
+        let points2 = Hashtbl.find cycles connection2 in
+        List.iter (fun point -> Hashtbl.replace circuits point connection1) points2;
+        Hashtbl.replace cycles connection1 (points1 @ points2);
+        Hashtbl.remove cycles connection2;
+      end;
+    i := !i + 1;
+  done;
+
+  x_of (fst pairs.(!i)) * x_of (snd pairs.(!i))
+
 
 let () =
-  "test.txt"
+  "prod.txt"
   |> lines_of_file
   |> Seq.map (fun line -> Scanf.sscanf line "%d,%d,%d" (fun x y z -> (x, y, z)))
-  |> List.of_seq
-  |> solution1 10
+  |> Array.of_seq
+  |> solution2
   |> Printf.printf "%d\n"
